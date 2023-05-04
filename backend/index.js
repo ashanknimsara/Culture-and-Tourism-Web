b84b21b5ad0a46acd7d952986fefc964c7f101c9
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-05-04 10:33:08 +0530

    backend struct1

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const cors = require("cors");
+const express = require('express');
+const bodyparser = require('body-parser')
+const { success, error } = require("consola");
+const { connect } = require('mongoose')
+
+const { DB, PORT } = require('./config')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+
+
+//routes
+//Travel Blog
+
+//Accommodations
+
+//Travel Agencies
+
+//Tour Guide
+
+
+const startApp = async () => {
+    try {
+        await connect(DB);
+        success({
+            message: `Successfully connected with the Database ${DB}`,
+            badge: true
+        })
+
+        app.listen(PORT, () => success({ message: `Server started on PORT ${PORT}`, badge: true }))
+    } catch (err) {
+
+        error({
+            message: `Unable to connect with the Database ${DB}`,
+            badge: true
+        })
+        startApp();
+    }
+
+
+}
+
+startApp();
