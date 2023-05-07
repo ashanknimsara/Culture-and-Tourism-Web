44d3bb1249240d5ba30bc9e0d785fdc2cd416f68
bino1999 <101051395+bino1999@users.noreply.github.com>
2023-05-07 10:14:33 +0530

    modify
    guide router,model,form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,16 @@ import Home from "./components/LandingPage";
 import TravelBlog from "./components/blog/TravelBlog";
 import Accomodations from "./components/Accomodations";
 import TourGuides from "./components/TourGuides";
-import AddAgency from './components/agencies/AddAgency';
+import AddAgency from "./components/agencies/AddAgency";
 import ViewAgencies from "./components/agencies/ViewAgencies";
 import AdminView from "./components/agencies/AdminView";
 
-import AddBlogPost from "./components/blog/AddBlogPost"
-import Login from "./components/Login"
-import NewAdmin from "./components/SignUp"
+import AddBlogPost from "./components/blog/AddBlogPost";
+import Login from "./components/Login";
+import NewAdmin from "./components/SignUp";
 
 import UpdateAgency from "./components/agencies/UpdateAgency";
-
-
+import GuideForm from "./components/tourGuide/GuideForm";
 function App() {
   return (
     <div>
@@ -23,24 +22,22 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/accomodations" element={<Accomodations />} />
           <Route path="/tour-guides" element={<TourGuides />} />
-          <Route path="/AddAgency" element={<AddAgency/>}/>
-          <Route path="/ViewAgency" element={<ViewAgencies/>}/>
-          <Route path="/viewadmin" element={<AdminView/>}/>
+          <Route path="/AddAgency" element={<AddAgency />} />
+          <Route path="/ViewAgency" element={<ViewAgencies />} />
+          <Route path="/viewadmin" element={<AdminView />} />
 
-
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/login" element={<Login />} />
           <Route path="/admin/new-admin" element={<NewAdmin />} />
-          
+
           <Route path="/blog" element={<TravelBlog />} />
           <Route path="/newblogpost" element={<AddBlogPost />} />
-          
-          <Route path="/UpdateAgency/:id" element={<UpdateAgency/>}/>
-          
 
+          <Route path="/UpdateAgency/:id" element={<UpdateAgency />} />
+
+          <Route path="/enterGuide" element={<GuideForm />} />
         </Routes>
       </BrowserRouter>
     </div>
-
   );
 }
 
